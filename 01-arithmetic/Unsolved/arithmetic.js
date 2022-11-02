added4fd4dc224c4642fa5b830d7c52c11742c22
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-11-01 21:28:05 -0500

    week 3 algorithms

---
+++ 01-arithmetic/Unsolved/arithmetic.js
@@ -0,0 +1,26 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+  // in here is where you write the logic
+  return num1 + num2;
+};
+
+add(4,6);
+
+var subtract = function(num1, num2) {
+  return num1-num2;
+};
+
+subtract(100,99);
+
+var multiply = function(num1, num2) {
+  return num1 * num2;
+};
+
+multiply(10, 50);
+
+var divide = function(num1, num2) {
+  return num1 / num2;
+};
+
+divide(100,10);
